846406eaf530925cf5a534784617e3f14cb1e5cb
Jacob Menashi <jbmenashi@gmail.com>
2018-10-02 17:32:29 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,5 +17,11 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.include()
+  cheese_types.each do |cheese|
+    if array.include?(cheese)
+      cheese
+    else
+      nil
+    end
+  end
 end
